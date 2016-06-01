0fea714830cf9f3618723598c728cb67cb48176b
saranya Ravi <saranya.ravi@philips.com>
2016-06-01 23:00:27 +0200

    Update to energyviewcontroller

---
+++ MyHealthApp/EnergyViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnergyViewController.h
+//  MyHealthApp
+//
+//  Created by saranya.ravi@philips.com on 01/06/16.
+//  Copyright © 2016 saranya.ravi@philips.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnergyViewController : UIViewController
+
+@end
---
+++ MyHealthApp/EnergyViewController.m
@@ -0,0 +1,24 @@
+//
+//  EnergyViewController.m
+//  MyHealthApp
+//
+//  Created by saranya.ravi@philips.com on 01/06/16.
+//  Copyright © 2016 saranya.ravi@philips.com. All rights reserved.
+//
+
+#import "EnergyViewController.h"
+
+@interface EnergyViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *caloriesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *activeBurnLabel;
+@property (weak, nonatomic) IBOutlet UILabel *netLabel;
+
+@end
+
+@implementation EnergyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
